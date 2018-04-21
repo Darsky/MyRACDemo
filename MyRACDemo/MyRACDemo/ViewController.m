d130a8b3990864f49008d7363925861374b67cb5
Darsky <276294393@qq.com>
2018-04-21 10:56:24 +0800

    2 examples

--- MyRACDemo/MyRACDemo/ViewController.m
+++ MyRACDemo/MyRACDemo/ViewController.m
@@ -7,18 +7,53 @@
 //
 
 #import "ViewController.h"
-
+#import "ReactiveObjC.h"
 @interface ViewController ()
+
+@property (copy, nonatomic) NSString *demoName;
+
+@property (copy, nonatomic) NSString *demoName2;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [RACObserve(self, demoName) subscribeNext:^(NSString *newName)
+     {
+        NSLog(@"%@", newName);
+    }];
+    
+    [[RACObserve(self, demoName2) filter:^BOOL(NSString *newName)
+     {
+         return [newName hasPrefix:@"World"];
+     }]
+     subscribeNext:^(id  _Nullable x)
+     {
+         NSLog(@"%@", x);
+     }];
 }
 
+
+- (IBAction)didSimpleDemoTouch:(id)sender
+{
+    self.demoName = @"Hello World";
+}
+
+- (IBAction)didChangeButtonTouch:(UIButton*)sender
+{
+    if (sender.tag == 0)
+    {
+        self.demoName2 = @"World";
+    }
+    else if (sender.tag == 1)
+    {
+        self.demoName2 = @"Earth";
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
